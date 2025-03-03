1c8127abb4c1b7338c54d0678f242b11fdb62900
ev0cativeaurora <s.anouar.edu@gmail.com>
2025-03-04 00:45:50 +0100

    Building Navbar

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+
+export default function Home() {
+  return (
+    <div className="m-4">
+      <h1>home page content</h1>
+    </div>
+  );
+}
