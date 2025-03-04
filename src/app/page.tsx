6d183c7d49b1ca073f75b98ed02c4769c493efb7
ev0cativeaurora <s.anouar.edu@gmail.com>
2025-03-04 12:13:31 +0100

    Authentication & Database sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-
-
-export default function Home() {
+export default async function Home() {
   return (
     <div className="m-4">
       <h1>home page content</h1>
